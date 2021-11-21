abe669ef7f5cab285730043989910a2b03098486
127niranjan <niranjan.reddy127@gmail.com>
2021-11-21 20:53:13 +0530

    my python files

---
+++ plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x = list(range(0,100))
+y = list(range(0,100))
+plt.plot(x,y)
+plt.show()
---
+++ sin.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy
+x = numpy.arange(0,10,0.1)
+s = numpy.sim(x)
+print(s)
+plt.plot(s)
+plt.show()
